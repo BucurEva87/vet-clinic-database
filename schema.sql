09c2f98ebb4b4c72aef2242ddc725b1553599ba6
BucurEva87 <bucur.eva87@gmail.com>
2022-11-04 22:58:46 +0200

    Add queries for requirements

--- schema.sql
+++ schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+CREATE TABLE owners (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
