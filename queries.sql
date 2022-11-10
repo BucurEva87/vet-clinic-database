f21007db5fa16e7dc94d5cb3f319c71ee5564464
BucurEva87 <bucur.eva87@gmail.com>
2022-11-10 15:24:45 +0200

    Add join tables functionality

--- queries.sql
+++ queries.sql
@@ -9,20 +9,6 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gaubumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-BEGIN;
-DELETE FROM animals;
-SELECT * FROM animals;
-ROLLBACK;
-SELECT * FROM animals;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-COMMIT;
-
 SELECT COUNT(id) FROM animals;
 SELECT COUNT(id) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
@@ -35,3 +21,13 @@ SELECT species.name, COUNT(animals.name) FROM animals JOIN species ON animals.sp
 SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell';
 SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 SELECT owners.full_name, COUNT(animals.name) FROM owners LEFT JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY COUNT(animals.name) DESC;
+
+SELECT animals.name FROM animals INNER JOIN visits ON animals.id = visits.animal_id INNER JOIN vets ON vets.id = visits.vet_id WHERE vets.name = 'William Tatcher' ORDER BY date_of_visit DESC LIMIT 1;
+SELECT COUNT(*) FROM (SELECT vets.name FROM visits JOIN vets ON visits.vet_id = vets.id WHERE vets.name = 'Maisy Smith' GROUP BY vets.name, visits.animal_id) AS table_of_visits;
+SELECT v.name, s2.name FROM vets AS v LEFT JOIN specializations AS s1 ON v.id = s1.vet_id LEFT JOIN species AS s2 ON s1.specie_id = s2.id ORDER BY v.id;
+SELECT DISTINCT animals.name FROM vets JOIN visits ON vets.id = visits.vet_id JOIN animals ON animals.id = visits.animal_id WHERE vets.name = 'Stephanie Mendez' AND visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+SELECT animals.name FROM visits JOIN animals ON visits.animal_id = animals.id GROUP BY animals.name ORDER BY COUNT(*) DESC LIMIT 1;
+SELECT animals.name FROM vets JOIN visits ON vets.id = visits.vet_id JOIN animals ON visits.animal_id = animals.id WHERE vets.name = 'Maisy Smith' ORDER BY visits.date_of_visit LIMIT 1;
+SELECT a.id AS animal_id, a.name AS animal_name, date_of_birth, neutered, weight_kg, v.id AS vet_id, v.name AS vet_name, age, date_of_graduation FROM animals AS a JOIN visits ON a.id = visits.animal_id JOIN vets AS v ON visits.vet_id = v.id;
+SELECT COUNT(*) FROM vets AS ve JOIN visits AS vi ON ve.id = vi.vet_id FULL JOIN specializations AS sz ON ve.id = sz.vet_id JOIN animals AS a ON vi.animal_id = a.id WHERE sz.specie_id IS NULL OR sz.specie_id != a.species_id;
+SELECT sp.name FROM vets AS ve JOIN visits AS vi ON ve.id = vi.vet_id JOIN animals AS a ON vi.animal_id = a.id JOIN species AS sp ON a.species_id = sp.id WHERE ve.name = 'Maisy Smith' GROUP BY sp.name ORDER BY COUNT(sp.name) DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,36 +1,48 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name varchar(100),
-    date_of_birth date,
-    escape_attempts int,
-    neutered boolean,
-    weight_kg decimal,
-    PRIMARY KEY(id)
-);
-
-ALTER TABLE animals ADD COLUMN species varchar(100);
-
-CREATE TABLE owners (
+CREATE TABLE IF NOT EXISTS owners (
     id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     full_name varchar(100),
     age INT,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE species (
+CREATE TABLE IF NOT EXISTS species (
     id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
     PRIMARY KEY(id)
 );
 
-ALTER TABLE animals DROP COLUMN species;
+CREATE TABLE IF NOT EXISTS animals (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal,
+    species_id int,
+    owner_id int,
+    fk_animals_species int,
+    fk_animals_owner int,
+    PRIMARY KEY(id),
+    FOREIGN KEY(species_id) REFERENCES species(id),
+    FOREIGN KEY(owner_id) REFERENCES owners(id)
+);
 
-ALTER TABLE animals ADD COLUMN species_id INT;
+CREATE TABLE IF NOT EXISTS vets (
+    id SERIAL PRIMARY KEY,
+    name varchar(100),
+    age int,
+    date_of_graduation date
+);
 
-ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
+CREATE TABLE IF NOT EXISTS specializations (
+    vet_id int,
+    specie_id int
+);
 
-ALTER TABLE animals ADD COLUMN owner_id INT;
-
-ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+CREATE TABLE IF NOT EXISTS visits (
+    vet_id int,
+    animal_id int,
+    date_of_visit date
+);
